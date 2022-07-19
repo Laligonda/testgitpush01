0050253da0e78b671a3462ffbf8756cfcfb06e3a
laligonda <laligondabaskarkumar@outlook.com>
2022-07-19 11:07:55 +0530

    this is my third code push in my git

--- atlas.py
+++ atlas.py
@@ -1,14 +1,12 @@
 import pymongo
-client = pymongo.MongoClient("mongodb+srv://dbBHK:Dsa123@cluster0.9eygmye.mongodb.net/?retryWrites=true&w=majority")
-db = client.test
-print(db)
 
-d = {
-    "name": "Bhaskar",
-    "email": "abc@gmail.com",
-    "lastname": "kumar"
-}
 
-db1 = client['mongotest']
-coll = db1['test']
-coll.insert_one(d)
+# d = {
+#     "name": "Bhaskar",
+#     "email": "abc@gmail.com",
+#     "lastname": "kumar"
+# }
+#
+# db1 = client['mongotest']
+# coll = db1['test']
+# coll.insert_one(d)
